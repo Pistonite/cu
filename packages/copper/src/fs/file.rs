2c756f5bd39dc6ec3706d6ae4845741d00197d5b
Pistonight <pistonknight@outlook.com>
2025-09-04 18:58:15 -0700

    add fs::copy and improve cargo preset error message handling

--- packages/copper/src/fs/file.rs
+++ packages/copper/src/fs/file.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::pre::*;
 
@@ -15,6 +15,81 @@ pub fn current_exe() -> crate::Result<PathBuf> {
     std::env::current_exe().context("failed to get current exe path")
 }
 
-// WIP
-// Move a file from
-// pub fn move_file(from: impl AsRef<Path>, to: impl AsRef<Path>)
+/// Copy a file from one to another.
+///
+/// If copy failed, it will attempt to fallback to using read and write.
+/// Directories will be created for the target location if not already exists.
+///
+/// if from and to is the pointing to the same, it might be truncated.
+///
+/// The number of bytes in `to` is returned.
+pub fn copy(from: impl AsRef<Path>, to: impl AsRef<Path>) -> crate::Result<u64> {
+    let from = from.as_ref();
+    let to = to.as_ref();
+    crate::trace!("copy from='{}' to='{}'", from.display(), to.display());
+    if to.is_dir() {
+        crate::bail!(
+            "cannot copy from '{}' to '{}': target is directory",
+            from.display(),
+            to.display()
+        );
+    }
+    let copy_error = match std::fs::copy(from, to) {
+        Ok(v) => return Ok(v),
+        Err(e) => e,
+    };
+    // we know the fallback will also fail in these cases
+    if !from.exists() {
+        crate::rethrow!(
+            copy_error,
+            "cannot copy from '{}' to '{}': source does not exist",
+            from.display(),
+            to.display()
+        );
+    }
+    if from.is_dir() {
+        crate::rethrow!(
+            copy_error,
+            "cannot copy from '{}' to '{}': source is directory",
+            from.display(),
+            to.display()
+        );
+    }
+    // try the fallback
+    crate::trace!(
+        "copy failed, attempting fallback, from='{}' to='{}'",
+        from.display(),
+        to.display()
+    );
+    let bytes = match super::read(from) {
+        Err(e) => {
+            crate::trace!(
+                "fallback copy failed when reading '{}': {e:?}",
+                from.display()
+            );
+            crate::rethrow!(
+                copy_error,
+                "failed to copy file from '{}' to '{}'",
+                from.display(),
+                to.display()
+            );
+        }
+        Ok(x) => x,
+    };
+    let size = bytes.len() as u64;
+    match super::write(to, bytes) {
+        Err(e) => {
+            crate::trace!(
+                "fallback copy failed when writing '{}': {e:?}",
+                to.display()
+            );
+            crate::rethrow!(
+                copy_error,
+                "failed to copy file from '{}' to '{}'",
+                from.display(),
+                to.display()
+            );
+        }
+        Ok(_) => Ok(size),
+    }
+}
