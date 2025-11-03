977b5b008095bf1841a810c5689be28086952abb
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-11-02 18:10:05 -0800

    feat: add env var reading with reporting (#16)

--- packages/copper/src/env_var.rs
+++ packages/copper/src/env_var.rs
@@ -1,23 +1,31 @@
-use std::path::PathBuf;
+use std::ffi::OsStr;
 
-use crate::{bail, Context as _, Parse, Result};
+use crate::pre::*;
 
-/// Read an environment variable and automatically parses it
+/// Like [`std::env::var`], but treat not-set as empty string.
+/// Tracing and reporting is built-in.
 ///
-/// If the var is not set, it's treated as an empty string
-pub fn env_var<T: Parse>(var: impl AsRef<str>) -> Result<T::Output> {
-    let var = var.as_ref();
-    log::trace!("reading env var `{var}`");
-    let value = match std::env::var(var) {
-        Ok(v) => v,
-        Err(std::env::VarError::NotPresent) => String::new(),
-        Err(e) => {
-            log::error!("failed to read env var `{var}`: {e}");
-            Err(e).context(format!("failed to read env var `{var}`"))?
-        }
-    };
-    T::parse(value).with_context(|| {
-        format_log_error!("failed to parse env var `{var}`")
-    })
+/// If you need to detect the case where the env var is not set,
+/// use `std::env::var` directly.
+///
+/// ```rust
+/// # use pistonite_cu as cu;
+/// # fn main() -> cu::Result<()> {
+/// assert!(!cu::env_var("CARGO")?.is_empty());
+/// assert!(cu::env_var("NOT_SET")?.is_empty());
+/// # Ok(()) }
+/// ```
+#[inline(always)]
+pub fn env_var(var: impl AsRef<OsStr>) -> crate::Result<String> {
+    env_var_impl(var.as_ref())
 }
-
+fn env_var_impl(var: &OsStr) -> crate::Result<String> {
+    crate::trace!("reading env var '{}`", var.display());
+    match std::env::var(var) {
+        Ok(v) => Ok(v),
+        Err(std::env::VarError::NotPresent) => Ok(String::new()),
+        Err(e) => {
+            crate::rethrow!(e, "failed to read env var '{}'", var.display());
+        }
+    }
+}
