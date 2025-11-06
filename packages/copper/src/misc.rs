7d77ad6b6e0245e25a0dcd853d689c685f85bcc6
Pistonight <pistonknight@outlook.com>
2025-11-05 19:40:49 -0800

    wrapper for std::io::copy and tokio::io::copy

--- packages/copper/src/misc.rs
+++ packages/copper/src/misc.rs
@@ -1,5 +1,7 @@
 use std::any::Any;
 
+use crate::Context as _;
+
 /// Try to get info from a panic payload
 pub fn best_effort_panic_info<'a>(payload: &'a Box<dyn Any + Send + 'static>) -> &'a str {
     if let Some(s) = payload.downcast_ref::<&str>() {
@@ -26,6 +28,33 @@ macro_rules! noimpl {
         let msg = format!("not implemented: {}", format_args!($(args)*));
         $crate::bailand!(error!("{msg}"))
     }}
+}
+
+/// Copy a reader to a writer.
+///
+/// This is wrapper for `std::io::copy` with error context
+#[inline(always)]
+pub fn copy<R, W>(reader: &mut R, writer: &mut W) -> crate::Result<u64>
+where
+    R: std::io::Read + ?Sized,
+    W: std::io::Write + ?Sized,
+{
+    std::io::copy(reader, writer).context("stream copy failed")
+}
+
+/// Copy an async reader to an async writer.
+///
+/// This is wrapper for `tokio::io::copy` with error context
+#[cfg(feature = "coroutine")]
+#[inline(always)]
+pub async fn co_copy<'a, R, W>(reader: &'a mut R, writer: &'a mut W) -> crate::Result<u64>
+where
+    R: tokio::io::AsyncRead + Unpin + ?Sized,
+    W: tokio::io::AsyncWrite + Unpin + ?Sized,
+{
+    tokio::io::copy(reader, writer)
+        .await
+        .context("async stream copy failed")
 }
 
 /// Check a `Result`, unwrapping the value or giving it a context
